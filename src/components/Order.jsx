2a5f05a1cbb0505f72f383492f3ef03b0fcbe3a5
Qadir <qadir.qanbarpoor@gmail.com>
2024-12-22 20:07:58 -0800

    0

---
+++ src/components/Order.jsx
@@ -0,0 +1,10 @@
+const Order = ({ closeHour }) => {
+  return (
+    <div className="order">
+      <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+      <button className="btn">Order</button>
+    </div>
+  );
+};
+
+export default Order;
