6c9126712ca464c69fe69dff97741ee6c2450a06
Qadir <qadir.qanbarpoor@gmail.com>
2025-02-14 05:56:18 -0800

    0

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Menu from "./components/Menu";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import Order from "./Order";
+
+const Footer = () => {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour < closeHour;
+
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <Order closeHour={closeHour} />
+      ) : (
+        <p>
+          We're work between {openHour}:00 still {closeHour}:00
+        </p>
+      )}
+    </footer>
+  );
+};
+
+export default Footer;
